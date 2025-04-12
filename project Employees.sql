7f136766c3abb9a27c408e3c6b4c832afe3f8983
nafisa <nafisa@jashim.com>
2025-04-12 20:16:27 +0600

    project employees

---
+++ project Employees.sql
@@ -0,0 +1,5 @@
+select p.project_id, ifnull(round(sum(e.experience_years)/ count(p.project_id),2),0) as average_years
+from project p
+left join employee e
+on p.employee_id = e.employee_id
+group by project_id 
